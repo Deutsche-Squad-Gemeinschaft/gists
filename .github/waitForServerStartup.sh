9d730a352ac18d1fd61621cffba06b3e5252f45d
Patrick <privat@skyraptor.eu>
2021-06-11 10:05:23 +0200

    Update waitForServerStartup.sh
    Increase wait for startup to 30 minutes as updates can take rather long

--- .github/waitForServerStartup.sh
+++ .github/waitForServerStartup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-timeout --signal=SIGINT 900 docker logs -f squad-server 2>&1 | grep -qe "LogOnlineSession"
+timeout --signal=SIGINT 1800 docker logs -f squad-server 2>&1 | grep -qe "LogOnlineSession"
 
 if [ $? == 1 ]; then
     echo "Server startup did timeout."
