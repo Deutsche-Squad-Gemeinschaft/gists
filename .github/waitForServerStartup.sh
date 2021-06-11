30a8fcd5fb35eeed8d378edad8b7288f10c063d0
Patrick <privat@skyraptor.eu>
2021-06-11 10:36:57 +0200

    Update waitForServerStartup.sh

--- .github/waitForServerStartup.sh
+++ .github/waitForServerStartup.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
 function waitForStartup() {
-  local MAXSTEPS=$(($1 / 10))
-  
-  for i in $(seq 1 $MAXSTEPS); do
-    timeout --signal=SIGINT 10 docker logs -f squad-server 2>&1 | grep -qe "LogOnlineSession"
-    
-    if [ $? == 1 ]; then
-      if [ "$i" -gt "$MAXSTEPS" ]; then
-        # Notify of failure and break
-        echo "Server startup did timeout :("
-        exit 1
+  if [ "$( docker container inspect -f '{{.State.Status}}' squad-server )" == "running" ]; then
+    local MAXSTEPS=$(($1 / 10))
+
+    for i in $(seq 1 $MAXSTEPS); do
+      timeout --signal=SIGINT 10 docker logs -f squad-server 2>&1 | grep -qe "LogOnlineSession"
+
+      if [ $? == 1 ]; then
+        if [ "$i" -gt "$MAXSTEPS" ]; then
+          # Notify of failure and break
+          echo "Server startup did timeout :("
+          exit 1
+        else
+          # Show a sign that we are alive
+          echo "Waiting for server startup... ($i/$MAXSTEPS). Last Log: \"${docker logs -n 1 squad-server 2>&1}\""
+        fi
       else
-        # Show a sign that we are alive
-        echo "Waiting for server startup... ($i/$MAXSTEPS)"
+        # Server seems to have started, break the loop
+        echo "Server Started!"
+        break
       fi
-    else
-      # Server seems to have started, break the loop
-      echo "Server Started!"
-      break
-    fi
-  done
+    done
+  else
+    echo "No container named squad-server is started!"
+    exit 1
+  fi
 }
 
 waitForStartup 1800
