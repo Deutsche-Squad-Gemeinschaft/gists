55d359b6482b777a7ba2542329a059977452715b
Patrick <privat@skyraptor.eu>
2021-06-11 10:11:43 +0200

    Add some more log output to waitForServerStartup

--- .github/waitForServerStartup.sh
+++ .github/waitForServerStartup.sh
@@ -1,8 +1,26 @@
-#!/bin/bash
-timeout --signal=SIGINT 1800 docker logs -f squad-server 2>&1 | grep -qe "LogOnlineSession"
+#!/usr/bin/env bash
 
-if [ $? == 1 ]; then
-    echo "Server startup did timeout."
-    docker logs squad-server
-    exit 1
-fi
+function waitForStartup() {
+  local MAXSTEPS=$(($1 / 10))
+  
+  for i in $(seq 1 $MAXSTEPS); do
+    timeout --signal=SIGINT 10 docker logs -f squad-server 2>&1 | grep -qe "LogOnlineSession"
+    
+    if [ $? == 1 ]; then
+      if [ "$i" -gt "$MAXSTEPS" ]; then
+        # Notify of failure and break
+        echo "Server startup did timeout :("
+        exit 1
+      else
+        # Show a sign that we are alive
+        echo "Waiting for server startup... ($i/$MAXSTEPS)"
+      fi
+    else
+      # Server seems to have started, break the loop
+      echo "Server Started!"
+      break
+    fi
+  done
+}
+
+waitForStartup 1800
